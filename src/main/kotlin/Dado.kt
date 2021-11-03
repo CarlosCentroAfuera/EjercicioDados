6790e0a9cee354a182823465cc5727e02520c7db
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-11-03 13:26:09 +0100

    Ejercicio solucionado

--- src/main/kotlin/Dado.kt
+++ src/main/kotlin/Dado.kt
@@ -1,8 +1,10 @@
 import kotlin.random.Random
 
-class Dado {
-    private var numMin = 0
-    private var numMax = 10
+class Dado(var numMin: Int = 1, var numMax: Int = 6) {
+
+    init {
+        darValores(numMin, numMax)
+    }
 
     fun darValores(valMin: Int, valMax: Int) {
         if (valMin <= valMax) {
@@ -16,14 +18,7 @@ class Dado {
     }
 
     fun tiradaUnica (): Int {
-        return Random.nextInt(numMin, numMax)
+        return Random.nextInt(numMin, numMax + 1)
     }
 
-    fun tiradaDoble (): Int {
-        val num1 = Random.nextInt(numMin, numMax)
-        println("num1 = $num1")
-        val num2 = Random.nextInt(numMin, numMax)
-        println("num2 = $num2")
-        return if (num1 == num2) num1 * num2 else num1 + num2
-    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,39 @@
+var listaOrdenada = listOf<Jugador>()
 fun main() {
     val dado1 = Dado()
-    val dado2 = Dado()
+    val dado2 = Dado(1,3)
+    val dado3 = Dado(4,6)
 
-    dado1.darValores(3,4)
-    dado2.darValores(0,30)
 
-    println("El dado1 ha obtenido un en tiradaUnica ${dado1.tiradaUnica()}")
-    println("El dado1 ha obtenido un en tiradaDoble ${dado1.tiradaDoble()}")
+    val listaJugadores = List(100){
+        Jugador(it.toString(), dado1,dado2,dado3)
+    }
 
-    println("El dado2 ha obtenido un en tiradaUnica ${dado2.tiradaUnica()}")
-    println("El dado2 ha obtenido un en tiradaDoble ${dado2.tiradaDoble()}")
+    listaOrdenada = listaJugadores.sortedByDescending { it.sumaTotal }
 
+
+    println(mostrarResultado())
+}
+
+fun mostrarResultado():String {
+    var output = ""
+    listaOrdenada.forEach { output += it.toString() + "\n" }
+    return output
+}
+
+class Jugador(val nombre: String, val dado1: Dado, val dado2: Dado, val dado3: Dado) {
+
+    var sumaTotal = 0
+
+    init {
+        repeat(100){
+            sumaTotal += dado1.tiradaUnica()
+            sumaTotal += dado2.tiradaUnica()
+            sumaTotal += dado3.tiradaUnica()
+        }
+    }
+
+    override fun toString(): String {
+        return "Jugador $nombre ha sacado $sumaTotal puntos"
+    }
 }
